97148d3a514f81206658c11874170cac15c0863f
erik <erik@DESKTOP-07MSP3L>
2024-01-11 00:58:23 -0600

    x

---
+++ JQueryDataTablesMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JQueryDataTablesMVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JQueryDataTablesMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Personas> Personas { get; set; }
+    }
+}
---
+++ JQueryDataTablesMVC/Models/Personas.cs
@@ -0,0 +1,9 @@
+﻿namespace JQueryDataTablesMVC.Models
+{
+    public class Personas
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+    }
+}
